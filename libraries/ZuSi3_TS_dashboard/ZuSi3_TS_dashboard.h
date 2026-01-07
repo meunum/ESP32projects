f952d4f1e4106759e88b8a132a51693efa237e9b
meunum <thomas.selbmann@gmx.de>
2026-01-07 16:57:28 +0100

    wifi-connection configureable

--- libraries/ZuSi3_TS_dashboard/ZuSi3_TS_dashboard.h
+++ libraries/ZuSi3_TS_dashboard/ZuSi3_TS_dashboard.h
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Arduino_JSON.h>
+#include <NetworkClient.h>
 #include <ZuSi3Schnittstelle.h>
 #include <ZuSi3_TS_controls.h>
 
@@ -11,26 +12,30 @@ extern float* G_AnalogInGPIOData;
 class ZuSi3_TS_DashBoard
 {
 public:
-  ZuSi3_TS_DashBoard();
-  void Init(int controlCount, int digitalPinCount, int analogPinCount);
-  void SetConfig(String config_json);
-  void SetBaureihe(String name);
-  void AddControl(ZuSi3_TS_Control* control);
-  void Update();
-  ZuSi3_TS_Control** Controls;
-  int ControlCount = 0;
-  int AnalogInGPIOLength = 0;
-  int DigitalOutGPIOLength = 0;
-  
+	ZuSi3_TS_DashBoard();
+	void InitNetwork(NetworkClient *client);
+	void SetConfig(String config_json);
+	void SetBaureihe(String name);
+	void AddControl(ZuSi3_TS_Control* control);
+	void Update();
+	ZuSi3_TS_Control** Controls;
+	int ControlCount = 0;
+	int AnalogInGPIOLength = 0;
+	int DigitalOutGPIOLength = 0;
+	
 private:
-  JSONVar parseConfig();
-  void loadHardwareConfig(JSONVar config);
-  void loadBaureihenConfig(JSONVar config, String baureihe);
-  String configJson = "";
-  String baureihe = "default";
-  int analogInGPIOLength = 0;
-  int digitalOutGPIOLength = 0;
-  int prevStufe = 0;
-  float prevAnalogValue = 0;
-  int motorHold = 0;
+	JSONVar parseConfig();
+	void loadSystemConfig(JSONVar config);
+	void loadHardwareConfig(JSONVar config);
+	void loadBaureihenConfig(JSONVar config, String baureihe);
+	String configJson = "";
+	String baureihe = "default";
+	int analogInGPIOLength = 0;
+	int digitalOutGPIOLength = 0;
+	int prevStufe = 0;
+	float prevAnalogValue = 0;
+	int motorHold = 0;
+	NetworkClient *networkClient;
+	String serverAdresse;
+	String serverPortnummer;
 };
